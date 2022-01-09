e807abd76f39104b94f0fd9d35e5633383923c5a
TheMystery <the-mystery@outlook.com>
2022-01-09 13:32:03 +1300

    Fixed auto reconnect
    Pleas let me know of any bugs (including session time not restarting)
    *Discord status will continue to count from when MCLink started

--- main.js
+++ main.js
@@ -48,11 +48,8 @@ const OPTIONS = {
 	brand: "Mineclub-Link", // Please don't change ♥
 };
 
-let BOT = mineflayer.createBot(OPTIONS);
-
-function login(){
-	BOT = mineflayer.createBot(OPTIONS);
-}
+const BOT = mineflayer.createBot(OPTIONS);
+bindEvents(BOT);
 
 // Webhook Settings
 const webhookInfo = {
@@ -89,214 +86,251 @@ function resetStats() {
 	stats.endTime = 0;
 }
 
-// Detect Join
-BOT.once("spawn", async () => {
-	// Set stats to 0 and set start time
-	stats.startTime = Date.now();
-	resetStats();
-	// State that bot has connected
-	console.log("Connected!");
-	// Load resource pack and set webhook info
-	BOT.acceptResourcePack();
-	webhookInfo.UUID = BOT.player.uuid;
-	webhookInfo.USERNAME = BOT.username;
-	// Manage Discord status (Currently Broken)
-	if (settings.discordStatus == true) {
-		startStatus(BOT.username, stats.startTime);
-	}
-	await sendWebhook("join", { webhookInfo });
-});
+function bindEvents(BOT) {
+	// Detect Join
+	BOT.once("spawn", async () => {
+		// Set stats to 0 and set start time
+		stats.startTime = Date.now();
+		resetStats();
+		// State that bot has connected
+		console.log("Connected!");
+		// Load resource pack and set webhook info
+		BOT.acceptResourcePack();
+		webhookInfo.UUID = BOT.player.uuid;
+		webhookInfo.USERNAME = BOT.username;
+		// Manage Discord status (Currently Broken)
+		if (settings.discordStatus == true) {
+			startStatus(BOT.username, stats.startTime);
+		}
+		await sendWebhook("join", { webhookInfo });
+	});
 
-// Detect System Messages
-BOT.on("messagestr", async (message, messagePosition) => {
-	if (messagePosition == "system") {
-		// Token earning detection
-		if (message.match(/[\W]* You won ([0-9]) (\w*) Token[s]?!/g) != null) {
-			let amount = Number.parseInt(message.replace(/[^0-9]+/, ""));
-			let season = message.replace(
-				/[\W]* You won ([0-9]) (\w*) Token[s]?!/g,
-				"$2"
-			);
-			if (season != stats.season) {
-				stats.season = season;
-			}
-			stats.totalTokensEarnt += amount;
-			stats.tokenTimesEarnt++;
-			if (settings.tokenAlerts.active == true) {
-				let msg = messageCreator("token", { amount, season, stats });
-				await sendWebhook("token", { msg, webhookInfo });
-				if (settings.logToConsole == true) {
-					console.log(`Collected ${amount} ${season} tokens!`);
+	// Detect System Messages
+	BOT.on("messagestr", async (message, messagePosition) => {
+		if (messagePosition == "system") {
+			// Token earning detection
+			if (
+				message.match(/[\W]* You won ([0-9]) (\w*) Token[s]?!/g) != null
+			) {
+				const AMOUNT = Number.parseInt(message.replace(/[^0-9]+/, ""));
+				const SEASON = message.replace(
+					/[\W]* You won ([0-9]) (\w*) Token[s]?!/g,
+					"$2"
+				);
+				if (SEASON != stats.season) {
+					stats.season = SEASON;
 				}
-			}
-		}
-		// Token message detection
-		if (message.includes("鳠")) {
-			stats.tokenMessages++;
-		}
-		// Gem message detection
-		if (message.includes("阵")) {
-			stats.activityGems += 50;
-			stats.totalGems += 50;
-			if (settings.gemAlerts.active == true) {
-				let msg = messageCreator("gems", { stats });
-				await sendWebhook("gems", { msg, webhookInfo });
-				if (settings.logToConsole == true) {
-					console.log("Earnt 50 gems!");
-				}
-			}
-		}
-	}
-	if (messagePosition == "chat") {
-		// DM Detection
-		if (
-			message.match(/[\W]+(\w+) -> ME: ([\w\W]+)/g) &&
-			settings.dmAlerts == true
-		) {
-			let msg = messageCreator("message", {
-				message: message.replace(/[\W]+(\w+) -> ME: ([\w\W]+)/g, "$2"),
-			});
-			let username = message.replace(
-				/[\W]+(\w+) -> ME: ([\w\W]+)/g,
-				"$1"
-			);
-			await sendWebhook("dm", { msg, username, webhookInfo });
-			if (settings.logToConsole == true) {
-				console.log(`${username} -> ME: ${msg}`);
-			}
-		}
-		// Market sold detection
-		if (message.includes("這")) {
-			const BUYER_REGEX = RegExp(/[\W]+Purchase made by: [\W]+([\w]+)/g);
-			const BUYER_RESULT = BUYER_REGEX.exec(message);
-			const amount = Number.parseInt(message.replace(/[^0-9]+/, ""));
-			const buyer = BUYER_RESULT[1];
-			stats.marketGems += amount;
-			stats.totalGems += amount;
-			if (settings.marketAlerts.sellAlert == true) {
-				let msg = messageCreator("marketSold", {
-					amount,
-					buyer,
-					stats,
-				});
-				await sendWebhook("marketSold", { msg, webhookInfo });
-				if (settings.logToConsole == true) {
-					console.log(`${username} brought your item for ${amount}`);
-				}
-			}
-		}
-		// Market outbid detection
-		if (
-			message.match(
-				/[\W]\[(?:Market|Housing)] You have been outbid by \W+(\w+) ([\d,]+)/
-			)
-		) {
-			let username = message.replace(
-				/[\W]\[(?:Market|Housing)] You have been outbid by \W+(\w+) ([\d,]+)/,
-				"$1"
-			);
-			let amount = Number.parseInt(message.replace(/[^0-9]+/, ""));
-			const outbidSettings = settings.marketAlerts.outbidAlert;
-			if (outbidSettings.active == true) {
-				let msg = messageCreator("marketOutbid", { amount, username });
-				if (outbidSettings.ping == true && outbidSettings.pingUserID) {
-					await sendWebhook("marketOutbid", {
-						msg,
-						ping: `<@${outbidSettings.pingUserID}>`,
-						webhookInfo,
+				stats.totalTokensEarnt += AMOUNT;
+				stats.tokenTimesEarnt++;
+				if (settings.tokenAlerts.active == true) {
+					const MSG = messageCreator("token", {
+						amount: AMOUNT,
+						season: SEASON,
+						stats,
 					});
-				} else {
-					await sendWebhook("marketOutbid", { msg, webhookInfo });
+					await sendWebhook("token", { msg: MSG, webhookInfo });
+					if (settings.logToConsole == true) {
+						console.log(`Collected ${AMOUNT} ${SEASON} tokens!`);
+					}
 				}
-				if (settings.logToConsole == true) {
-					console.log(`Outbid by: ${username}. New price ${amount}`);
+			}
+			// Token message detection
+			if (message.includes("鳠")) {
+				stats.tokenMessages++;
+			}
+			// Gem message detection
+			if (message.includes("阵")) {
+				stats.activityGems += 50;
+				stats.totalGems += 50;
+				if (settings.gemAlerts.active == true) {
+					const MSG = messageCreator("gems", { stats });
+					await sendWebhook("gems", { msg: MSG, webhookInfo });
+					if (settings.logToConsole == true) {
+						console.log("Earnt 50 gems!");
+					}
 				}
 			}
 		}
-	}
-});
-
-// Detect Chat Messages
-BOT.on("chat", async (username, message) => {
-	if (username == BOT.username) {
-		return;
-	}
-	// Mention detection
-	if (
-		(message.includes(BOT.username) &&
-			settings.mentionAlerts.personal == true) ||
-		(message.includes("@everyone") &&
-			settings.mentionAlerts.everyone == true)
-	) {
-		let msg = messageCreator("message", { message });
-		await sendWebhook("mention", { msg, username, webhookInfo });
-		if (settings.logToConsole == true) {
-			console.log(`${username}: ${message}`);
+		if (messagePosition == "chat") {
+			// DM Detection
+			if (
+				message.match(/[\W]+(\w+) -> ME: ([\w\W]+)/g) &&
+				settings.dmAlerts == true
+			) {
+				const MSG = messageCreator("message", {
+					message: message.replace(
+						/[\W]+(\w+) -> ME: ([\w\W]+)/g,
+						"$2"
+					),
+				});
+				const USERNAME = message.replace(
+					/[\W]+(\w+) -> ME: ([\w\W]+)/g,
+					"$1"
+				);
+				await sendWebhook("dm", {
+					msg: MSG,
+					username: USERNAME,
+					webhookInfo,
+				});
+				if (settings.logToConsole == true) {
+					console.log(`${USERNAME} -> ME: ${MSG}`);
+				}
+			}
+			// Market sold detection
+			if (message.includes("這")) {
+				const BUYER_REGEX = RegExp(
+					/[\W]+Purchase made by: [\W]+([\w]+)/g
+				);
+				const BUYER_RESULT = BUYER_REGEX.exec(message);
+				const amount = Number.parseInt(message.replace(/[^0-9]+/, ""));
+				const buyer = BUYER_RESULT[1];
+				stats.marketGems += amount;
+				stats.totalGems += amount;
+				if (settings.marketAlerts.sellAlert == true) {
+					const MSG = messageCreator("marketSold", {
+						amount,
+						buyer,
+						stats,
+					});
+					await sendWebhook("marketSold", { msg: MSG, webhookInfo });
+					if (settings.logToConsole == true) {
+						console.log(
+							`${username} brought your item for ${amount}`
+						);
+					}
+				}
+			}
+			// Market outbid detection
+			if (
+				message.match(
+					/[\W]\[(?:Market|Housing)] You have been outbid by \W+(\w+) ([\d,]+)/
+				)
+			) {
+				const USERNAME = message.replace(
+					/[\W]\[(?:Market|Housing)] You have been outbid by \W+(\w+) ([\d,]+)/,
+					"$1"
+				);
+				const AMOUNT = Number.parseInt(message.replace(/[^0-9]+/, ""));
+				const outbidSettings = settings.marketAlerts.outbidAlert;
+				if (outbidSettings.active == true) {
+					const MSG = messageCreator("marketOutbid", {
+						amount: AMOUNT,
+						username: USERNAME,
+					});
+					if (
+						outbidSettings.ping == true &&
+						outbidSettings.pingUserID
+					) {
+						await sendWebhook("marketOutbid", {
+							msg: MSG,
+							ping: `<@${outbidSettings.pingUserID}>`,
+							webhookInfo,
+						});
+					} else {
+						await sendWebhook("marketOutbid", {
+							msg: MSG,
+							webhookInfo,
+						});
+					}
+					if (settings.logToConsole == true) {
+						console.log(
+							`Outbid by: ${USERNAME}. New price ${AMOUNT}`
+						);
+					}
+				}
+			}
 		}
-	}
-	// Goodnight detection
-	if (
-		message.match(/\bgoodnight\b/g) ||
-		message.match(/\bnight\b/g) ||
-		message.match(/\bnini\b/g) ||
-		message.match(/\bgn\b/g)
-	) {
-		stats.goodnights++;
-	}
-});
+	});
 
-// Close login window on join or close discord link window
-BOT.on("windowOpen", async (window) => {
-	if (window.title.includes("庳")) {
-		BOT.closeWindow(window.id);
-	} else if (window.title.includes("a")) {
-		BOT.closeWindow(window.id);
-	}
-});
+	// Detect Chat Messages
+	BOT.on("chat", async (username, message) => {
+		if (username == BOT.username) {
+			return;
+		}
+		// Mention detection
+		if (
+			(message.includes(BOT.username) &&
+				settings.mentionAlerts.personal == true) ||
+			(message.includes("@everyone") &&
+				settings.mentionAlerts.everyone == true)
+		) {
+			const MSG = messageCreator("message", { message });
+			await sendWebhook("mention", { msg: MSG, username, webhookInfo });
+			if (settings.logToConsole == true) {
+				console.log(`${username}: ${message}`);
+			}
+		}
+		// Goodnight detection
+		if (
+			message.match(/\bgoodnight\b/g) ||
+			message.match(/\bnight\b/g) ||
+			message.match(/\bnini\b/g) ||
+			message.match(/\bgn\b/g)
+		) {
+			stats.goodnights++;
+		}
+	});
 
-// Detect being kicked from the server
-let kicked = false;
-BOT.on("kicked", async (reason, loggedIn) => {
-	if (loggedIn) {
+	// Close login window on join or close discord link window
+	BOT.on("windowOpen", async (window) => {
+		if (window.title.includes("庳")) {
+			BOT.closeWindow(window.id);
+		} else if (window.title.includes("a")) {
+			BOT.closeWindow(window.id);
+		}
+	});
+
+	// Detect being kicked from the server
+	let kicked = false;
+	BOT.on("kicked", async (reason, loggedIn) => {
+		if (loggedIn) {
+			stats.endTime = Date.now();
+			const MSG = messageCreator("exit", { stats });
+			await sendWebhook("kick", { webhookInfo, reason, msg: MSG });
+		}
+		console.log(JSON.parse(reason).text);
+		kicked = true;
+	});
+
+	// Detect error with client
+	let crashed = false;
+	BOT.on("error", async (error) => {
+		if (error.code == "ECONNREFUSED") {
+			return console.log("Could not connect!");
+		}
+		if (error.code == "ECONNRESET") {
+			// Auto reconnect
+			const BOT = mineflayer.createBot(OPTIONS);
+			bindEvents(BOT);
+		} else {
+			stats.endTime = Date.now();
+			const MSG = messageCreator("exit", { stats });
+			await sendWebhook("crash", { webhookInfo, msg: MSG });
+			console.error(error);
+			crashed = true;
+		}
+	});
+
+	// Detect disconnecting from the server
+	BOT.on("end", async () => {
+		if (kicked || crashed) {
+			return;
+		}
 		stats.endTime = Date.now();
-		let msg = messageCreator("exit", { stats });
-		await sendWebhook("kick", { webhookInfo, reason, msg });
-	}
-	console.log(JSON.parse(reason).text);
-	kicked = true;
-});
+		const MSG = messageCreator("exit", { stats });
+		await sendWebhook("disconnect", { webhookInfo, msg: MSG });
+		console.log("Disconnected from server");
+		// Auto reconnect
+		const BOT = mineflayer.createBot(OPTIONS);
+		bindEvents(BOT);
+	});
 
-// Detect error with client
-let crashed = false;
-BOT.on("error", async (error) => {
-	if (error.code == "ECONNREFUSED") {
-		console.log("Could not connect!");
-	} else {
+	// Detect program stop
+	process.on("SIGINT", async function () {
 		stats.endTime = Date.now();
-		let msg = messageCreator("exit", { stats });
-		await sendWebhook("crash", { webhookInfo, msg });
-		console.error(error);
-		crashed = true;
-	}
-});
-
-// Detect disconnecting from the server
-BOT.on("end", async () => {
-	if (kicked || crashed) {
-		return;
-	}
-	stats.endTime = Date.now();
-	let msg = messageCreator("exit", { stats });
-	await sendWebhook("disconnect", { webhookInfo, msg });
-	console.log("Disconnected from server");
-	login(); //Auto reconnect
-});
-
-// Detect program stop
-process.on("SIGINT", async function () {
-	stats.endTime = Date.now();
-	let msg = messageCreator("exit", { stats });
-	await sendWebhook("disconnect", { webhookInfo, msg });
-	console.log("Disconnected from server");
-	process.exit();
-});
+		const MSG = messageCreator("exit", { stats });
+		await sendWebhook("disconnect", { webhookInfo, msg: MSG });
+		console.log("Disconnected from server");
+		process.exit();
+	});
+}
